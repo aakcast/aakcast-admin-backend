fe4aec3b593838b7695db6a4b9a97211e447555a
홍원기 <mankiplayer@gmail.com>
2022-02-16 12:45:22 +0900

    feat: apply swagger OpenAPI documentation

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,37 @@
 import { NestFactory, HttpAdapterHost } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { VersioningType, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './core/exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const serverApp = new FastifyAdapter();
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, serverApp);
+
+  // Set API version
+  app.enableVersioning({
+    defaultVersion: '1',
+    type: VersioningType.URI,
+  });
 
   // Set global pipe for validation
   app.useGlobalPipes(new ValidationPipe());
   // Set global exception filter
   const httpAdapterHost = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
+
+  // OpenAPI configuration
+  const config = new DocumentBuilder()
+    .setTitle('aakcast™ REST API backend')
+    .setDescription('REST API backend for aakcast™ admin')
+    .setVersion('1.0')
+    .addTag('Users', 'Manage users')
+    .addTag('Auth', 'Authentication including OAuth2')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   // Start listening requests
   await app.listen(3000);
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,6 +11,7 @@ import {
   SetMetadata,
   ParseIntPipe,
 } from '@nestjs/common';
+import { ApiTags, ApiBearerAuth, ApiOkResponse, ApiCreatedResponse, ApiNoContentResponse } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 import {
   CreateUserDto,
@@ -21,7 +22,13 @@ import {
 } from './dto';
 
 @Controller('users')
+@ApiTags('Users')
 export class UsersController {
+  /**
+   * Constructor
+   *
+   * @param usersService  Injected instance of UsersService
+   */
   constructor(private readonly usersService: UsersService) {}
 
   // login/logout
@@ -40,21 +47,26 @@ export class UsersController {
   // }
 
   @Post()
+  @ApiCreatedResponse({ description: 'User has been successfully created.' })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
-  findAll(@Query() query: ListAllUsers) {
+  @ApiOkResponse()
+  find(@Query() query: ListAllUsers) {
     return this.usersService.findAll(query);
   }
 
   @Get(':id')
+  @ApiOkResponse()
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiBearerAuth()
+  @ApiNoContentResponse()
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateUserDto: UpdateUserDto,
@@ -64,27 +76,32 @@ export class UsersController {
 
   @Delete(':id')
   @SetMetadata('roles', ['admin'])
+  @ApiBearerAuth()
+  @ApiNoContentResponse()
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.remove(id);
   }
 
   @Put(':id/activate')
   @SetMetadata('roles', ['admin'])
+  @ApiBearerAuth()
+  @ApiNoContentResponse()
   activate(@Param('id', ParseIntPipe) id: number) {
     // return this.usersService.setStatus(id, 'active');
   }
 
   @Put(':id/deactivate')
   @SetMetadata('roles', ['admin'])
+  @ApiBearerAuth()
+  @ApiNoContentResponse()
   deactivate(@Param('id', ParseIntPipe) id: number) {
     // return this.usersService.setStatus(id, 'inactive');
   }
 
   @Put(':id/reset-password')
+  @ApiNoContentResponse()
   resetPassword(
     @Param('id', ParseIntPipe) id: number,
     @Body() body: ResetPasswordDto,
-  ) {
-    return this.usersService.update(id, { password: body.password });
-  }
+  ) {}
 }
